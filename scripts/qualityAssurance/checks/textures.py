40b43ab18f2b2d7a1e4c6b0b182f852cc80d7729
adi766 <adityakumarteli7846@gmail.com>
2024-05-20 19:10:13 +0530

    Add lambert1 check

--- scripts/qualityAssurance/checks/textures.py
+++ scripts/qualityAssurance/checks/textures.py
@@ -3,6 +3,47 @@ from maya import cmds
 from ..utils import QualityAssurance, reference
 
 
+
+
+
+class Lambert1connections(QualityAssurance):
+    """
+    Animation curves will be checked to see if they are not set driven keys.
+    Non set driven keys should not be present in the scene and will be deleted
+    when fixing this error.
+    """
+    def __init__(self):
+        QualityAssurance.__init__(self)
+
+        self._name = "lambert1 has no incoming texture"
+        self._message = "{0} texture node(s) connected to lambert1"
+        self._categories = ["Textures"]
+        self._selectable = True
+
+    # ------------------------------------------------------------------------
+
+    def _find(self):
+        """
+        :return: lambert1 incoming connections
+        :rtype: generator
+        """
+        incoming_connections = cmds.listConnections('lambert1', destination=False, source=True)
+
+        if incoming_connections:
+            for connection in incoming_connections:
+                yield connection
+
+    def _fix(self, fileNode):
+        """
+        :param str fileNode:
+        """
+        incoming_connections = cmds.listConnections('lambert1', destination=False, source=True)
+
+        if incoming_connections:
+            for connection in incoming_connections:
+                cmds.delete(connection)
+
+
 class NonExistingTextures(QualityAssurance):
     """
     Animation curves will be checked to see if they are not set driven keys.
